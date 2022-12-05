165124e7f35cf47b8534eff776624c17dea9f2d0
Luke Moon <lukemoon88@yahoo.co.uk>
2022-12-05 10:31:19 +0000

    added Film class

--- src/main/java/UIElements/demo/Film.java
+++ src/main/java/UIElements/demo/Film.java
@@ -12,5 +12,46 @@ public class Film {
 
     @Column(name="title")
     String film_title;
-    
+
+    @Column(name="description")
+    String film_description;
+
+    @Column(name="release_year")
+    int release_year;
+
+    public Film() {
+
+    }
+
+    public int getFilm_id() {
+        return film_id;
+    }
+
+    public void setFilm_id(int film_id) {
+        this.film_id = film_id;
+    }
+
+    public String getFilm_description() {
+        return film_description;
+    }
+
+    public void setFilm_description(String film_description) {
+        this.film_description = film_description;
+    }
+
+    public String getFilm_title() {
+        return film_title;
+    }
+
+    public void setFilm_title(String film_title) {
+        this.film_title = film_title;
+    }
+
+    public int getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(int release_year) {
+        this.release_year = release_year;
+    }
 }
